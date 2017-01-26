3ef38b8e2b51c583c9d76d605a69b133f51f4149
mhesham90 <mohammedhesham.1990@gmail.com>
2017-01-26 18:31:56 +0200

    fxml server gui

---
+++ Server/src/server/FXMLDocumentController.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ * FXML Controller class
+ *
+ * @author mhesham
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private ToggleGroup myToggleGroup;
+    @FXML
+    private TableView<Players> tableView;
+    @FXML
+    private TableColumn idColumn;
+    @FXML
+    private TableColumn fNameColumn;
+    @FXML
+    private TableColumn lNameColumn;
+    @FXML
+    private TableColumn loginColumn;
+    @FXML
+    private TableColumn scoreColumn;
+    @FXML
+    private ObservableList<Players> data;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        idColumn.setCellValueFactory(
+            new PropertyValueFactory<Players,Integer>("id")
+        );
+        fNameColumn.setCellValueFactory(
+            new PropertyValueFactory<Players,String>("fname")
+        );
+        lNameColumn.setCellValueFactory(
+            new PropertyValueFactory<Players,String>("lname")
+        );
+        loginColumn.setCellValueFactory(
+            new PropertyValueFactory<Players,String>("username")
+        );
+        scoreColumn.setCellValueFactory(
+            new PropertyValueFactory<Players,String>("score")
+        );
+        data = FXCollections.observableArrayList();
+        tableView.setItems(data);
+    }
+
+    @FXML
+    protected void handleToggleOnAction(ActionEvent t) {
+        System.out.println("on");
+    }
+
+    @FXML
+    protected void handleToggleOffAction(ActionEvent t) {
+        System.out.println("off");
+    }
+    public void addPlayers(){
+    //getallplayers method
+    }
+
+}
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -6,11 +6,10 @@
 package server;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,17 +19,18 @@ import javafx.stage.Stage;
 public class Server extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
+        Scene scene = new Scene(root);
         
-        
-        
+        stage.setScene(scene);
+        stage.show();
     }
 
-    
-    
-    
-    
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
