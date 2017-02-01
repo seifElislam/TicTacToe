efaa3361637c353bee20310e8a8b56b101d25b1f
EhabGamal <ehab.g.sh@gmail.com>
2017-02-01 22:06:14 +0200

    Refactoring assets for both server and client

---
+++ Client/src/assets/Message.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assets;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Ehab
+ */
+public class Message implements Serializable{
+    private MsgType type;
+    private HashMap<String,String> data;
+    
+    public Message(MsgType type){
+        this.type = type;
+        data = new HashMap<String,String>();
+    }
+    public Message(MsgType type, int DestID){
+        this.type = type;
+        data.put("destid", String.valueOf(DestID));
+    }
+    public void setType(MsgType type){
+        this.type = type;
+    }
+    public MsgType getType(){ 
+        return type; 
+    }
+    public void setData(String key, String value){
+        data.put(key,value);
+    }
+    public String getData(String key){
+        if(data.containsKey(key))
+            return data.get(key);
+        else
+            return null;
+    }
+}
