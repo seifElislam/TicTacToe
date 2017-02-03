8f73f269ad565b074e4200ed20655c38b2b9db80
Seif El-Islam <seif.eleslam1990@gmail.com>
2017-02-03 12:18:12 +0200

    repair GUI

---
+++ Client/src/client/controllers/GameController.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.net.URL;
+import static java.time.Instant.now;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author seif
+ */
+public class GameController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML private Button b1,b2,b3,b4,b5,b6,b7,b8,b9,send,surrend;
+    @FXML private TextField txt_field;
+    @FXML int flag1=0,flag2=0,flag3=0,flag4=0,flag5=0,flag6=0,flag7=0,flag8=0,flag9=0;
+    @FXML String src="x.png";
+    @FXML  Image img = new Image(getClass().getResourceAsStream(src));
+    @FXML private Label player1Name,player2Name,massge,time;
+    @FXML private TextArea txt_area;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        player1Name.setText("seif");
+        player2Name.setText("ehab");
+        
+    }   
+    @FXML protected void handleButton_send_Action(ActionEvent event) {
+      System.out.println("send");
+      txt_area.appendText(player1Name.getText()+" : "+txt_field.getText() + "\n");
+      txt_field.setText("");
+    }
+    @FXML protected void handleButton_surrend_Action(ActionEvent event) {
+      System.out.println("surrend");
+    }
+     @FXML protected void handleButton1Action(ActionEvent event) {
+       if(flag1==0)
+        {
+            b1.setGraphic(new ImageView(img));
+            
+            flag1=1;
+        }
+       
+//      
+    }
+    @FXML protected void handleButton2Action(ActionEvent event) {
+      if(flag2==0)
+        {
+            b2.setGraphic(new ImageView(img));
+            flag2=1;
+        }
+      else{}
+    }
+    @FXML protected void handleButton3Action(ActionEvent event) {
+      if(flag3==0)
+        {
+            b3.setGraphic(new ImageView(img));
+            flag3=1;
+        }
+    }
+    @FXML protected void handleButton4Action(ActionEvent event) {
+      if(flag4==0)
+        {
+            b4.setGraphic(new ImageView(img));
+            flag4=1;
+        }
+    }
+    @FXML protected void handleButton5Action(ActionEvent event) {
+       if(flag5==0)
+        {
+            b5.setGraphic(new ImageView(img));
+            flag5=1;
+        }
+    }
+    @FXML protected void handleButton6Action(ActionEvent event) {
+       if(flag6==0)
+        {
+            b6.setGraphic(new ImageView(img));
+            flag6=1;
+        }
+    }
+     @FXML protected void handleButton7Action(ActionEvent event) {
+       if(flag7==0)
+        {
+            b7.setGraphic(new ImageView(img));
+            flag7=1;
+        }
+    }
+     @FXML protected void handleButton8Action(ActionEvent event) {
+       if(flag8==0)
+        {
+            b8.setGraphic(new ImageView(img));
+            flag8=1;
+        }
+    }
+    @FXML protected void handleButton9Action(ActionEvent event) {
+       if(flag9==0)
+        {
+            b9.setGraphic(new ImageView(img));
+            flag9=1;
+        }
+    }
+    
+}
---
+++ Client/src/client/controllers/HomeController.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author seif
+ */
+
+public class HomeController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML private Button invite,logout,easy,meduim,hard; 
+    @FXML private Label opponentName,opponentWins,opponentLoses,opponentDraws;
+    @FXML private Label playerName,playerWins,playerLoses,playerDraws;
+  //  @FXML private TableView<Person> allPlayersTable;
+    @FXML private ImageView imgView;
+     @FXML private String src="o.png";
+    @FXML private Image playerImg= new Image(getClass().getResourceAsStream(src)); ;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        opponentInfo() ;
+        playerInfo() ;
+    }    
+    @FXML protected void handleButton_invite_Action(ActionEvent event) {
+      System.out.println("invite");
+      playerImg.getClass().getResourceAsStream("o.png");
+      
+       imgView.setImage(playerImg);
+       System.out.println(playerImg.impl_getUrl());
+       System.out.println(imgView.getImage());
+    }
+    @FXML protected void handleButton_logout_Action(ActionEvent event) {
+       System.out.println("logout");
+    }
+    @FXML protected void handleButton_easy_Action(ActionEvent event) {
+      System.out.println("easy");
+    }
+    @FXML protected void handleButton_medium_Action(ActionEvent event) {
+      System.out.println("medium");
+    }
+    @FXML protected void handleButton_hard_Action(ActionEvent event) {
+      System.out.println("hard");
+    }
+    @FXML protected void playerInfo() {
+      playerName.setText("seif");
+      playerWins.setText("1000");
+      playerLoses.setText("1000");
+      playerDraws.setText("1000");
+    }
+    @FXML protected void opponentInfo() {
+      opponentName.setText("ehab");
+     opponentWins.setText("1000");
+      opponentLoses.setText("1000");
+      opponentDraws.setText("1000");
+    }
+    
+}
